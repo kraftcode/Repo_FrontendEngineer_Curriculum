710088b05d2efd4e792842b821ab16849cab6fac
Daniel Jaeger <daniel.jaeger@epost-dev.de>
2017-01-25 14:19:44 +0100

    Added webpack-merge module to set up configuration splitting into separate files (initially just webpack.config.js and webpack.parts.js).

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,65 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-
+const merge = require('webpack-merge');
+const parts = require('./webpack.parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-const common = {
-  // Entry accepts a path or an object of entries.
-  // We'll be using the latter form given it's
-  // convenient with more complex configurations.
-  //
-  // Entries have to resolve to files! It relies on Node.js
-  // convention by default so if a directory contains *index.js*,
-  // it will resolve to that.
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo',
-    }),
-  ],
-};
-
-const developmentConfig = {
-  devServer: {
-    // Enable history API fallback so HTML5 History API based
-    // routing works. This is a good default that will come
-    // in handy in more complicated setups.
-    historyApiFallback: true,
-
-    // Unlike the cli flag, this doesn't set
-    // HotModuleReplacementPlugin!
-    hot: true,
-
-    // Don't refresh if hot loading fails. If you want
-    // refresh behavior, set inline: true instead.
-    hotOnly: true,
-
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
-
-    // Parse host and port from env to allow customization.
+const common = merge([
+  {
+    // Entry accepts a path or an object of entries.
+    // We'll be using the latter form given it's
+    // convenient with more complex configurations.
     //
-    // If you use Vagrant or Cloud9, set
-    // host: options.host || '0.0.0.0';
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default `localhost`.
-    host: process.env.HOST, // Defaults to `localhost`
-    port: process.env.PORT, // Defaults to 8080
-  },
-  plugins: [
-    // Enable multi-pass compilation for enhanced performance
-    // in larger projects. Good default.
-    new webpack.HotModuleReplacementPlugin({
-      // Disabled as this won't work with html-webpack-template yet
-      //multiStep: true,
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: true,
-        },
-      },
+    // Entries have to resolve to files! It relies on Node.js
+    // convention by default so if a directory contains *index.js*,
+    // it will resolve to that.
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
     ],
   },
-};
+]);
 
 module.exports = function(env) {
   if (env === 'production') {
-    return common;
+    return merge([
+      common,
+      parts.lintJavaScript({ paths: PATHS.app }),
+    ]);
   }
 
-  return Object.assign(
-      {},
-      common,
-      developmentConfig,
+  return merge([
+    common,
     {
-      plugins: common.plugins.concat(developmentConfig.plugins),
-    }
-    );
+      plugins: [
+        new webpack.NamedModulesPlugin(),
+      ],
+    },
+    parts.devServer({
+      // Customize host/port here if needed
+      host: process.env.HOST,
+      port: process.env.PORT,
+    }),
+    parts.lintJavaScript({
+      paths: PATHS.app,
+      options: {
+        // Emit warnings over errors to avoid crashing
+        // HMR on error.
+        emitWarning: true,
+      },
+    }),
+  ]);
 };
