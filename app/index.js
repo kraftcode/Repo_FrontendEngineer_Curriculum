06a1313ad9d6d5cfc60bdb7f335cafc033b69f9c
Daniel Jaeger <daniel.jaeger@epost-dev.de>
2017-02-23 17:08:42 +0100

    Fixed travis timezone issue and cleaned up index.js

--- app/index.js
+++ app/index.js
@@ -7,18 +7,6 @@ import AppComponent from './components/AppComponent.jsx';
 
 useStrict(true); //mobx - will not allow mutating the state outside of Actions
 
-let a = `"{\"0\":42,\"1\":\"{\\\"standardStartDate\\\":\\\"2015-03-04T14:05:06.000Z\\\",\\\"startDay\\\":\\\"Mi.\\\",\\\"startDate\\\":\\\"4. März 2015\\\",\\\"startTime\\\":\\\"15:05\\\",\\\"endDate\\\":\\\"\\\",\\\"endTime\\\":\\\"\\\",\\\"duration\\\":0,\\\"earnings\\\":\\\"€0,00\\\",\\\"id\\\":0}\",\"2\":\"{\\\"standardStartDate\\\":\\\"2016-07-08T13:05:06.000Z\\\",\\\"startDay\\\":\\\"Fr.\\\",\\\"startDate\\\":\\\"8. Juli 2016\\\",\\\"startTime\\\":\\\"15:05\\\",\\\"endDate\\\":\\\"\\\",\\\"endTime\\\":\\\"\\\",\\\"duration\\\":0,\\\"earnings\\\":\\\"€0,00\\\",\\\"id\\\":1}\"}"`;
-let b = `"{\"0\":42,\"1\":\"{\\\"standardStartDate\\\":\\\"2015-03-04T15:05:06.000Z\\\",\\\"startDay\\\":\\\"Mi.\\\",\\\"startDate\\\":\\\"4. März 2015\\\",\\\"startTime\\\":\\\"15:05\\\",\\\"endDate\\\":\\\"\\\",\\\"endTime\\\":\\\"\\\",\\\"duration\\\":0,\\\"earnings\\\":\\\"€0,00\\\",\\\"id\\\":0}\",\"2\":\"{\\\"standardStartDate\\\":\\\"2016-07-08T15:05:06.000Z\\\",\\\"startDay\\\":\\\"Fr.\\\",\\\"startDate\\\":\\\"8. Juli 2016\\\",\\\"startTime\\\":\\\"15:05\\\",\\\"endDate\\\":\\\"\\\",\\\"endTime\\\":\\\"\\\",\\\"duration\\\":0,\\\"earnings\\\":\\\"€0,00\\\",\\\"id\\\":1}\"}"`;
-
-for(let i = 0; i < a.length; i++){
-  if(a[i] !== b[i]){
-    console.log('ERROR: ', a[i], ' ', b[i], ' ', i);
-  }
-}
-
-console.log(a === b);
-
-
 const patch = snabbdom.init([
   require('snabbdom/modules/class').default,
   require('snabbdom/modules/props').default,
